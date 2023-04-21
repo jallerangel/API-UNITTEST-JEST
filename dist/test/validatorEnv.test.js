8290f785555ca56cfa5275e27f3a3777c4266a1f
Luis G. Jaller <jallerangel06@gmail.com>
2023-04-21 16:57:40 -0500

    Test to validatorEnv Implemetation

--- dist/test/validatorEnv.test.js
+++ dist/test/validatorEnv.test.js
@@ -1,7 +1,38 @@
 "use strict";
-const configMock = {
-    dbNameMock: '',
-    dbUser: 'SAMUsak',
-    port: 8000
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+const validatorEnv_1 = __importDefault(require("../utils/validatorEnv"));
+describe('validatorEnv function', () => {
+    it('should return an error and a specific message', () => {
+        const configMock = {
+            dbNameMock: '',
+            dbUser: 'SAMUsak',
+            port: "8000"
+        };
+        const messageErrMock = 'Server cannot be initialized because it needs an environment variable.';
+        const expectedResult = new Error(messageErrMock);
+        expect(() => { (0, validatorEnv_1.default)(configMock); }).toThrow(expectedResult);
+    });
+    it('should return an error and a specific message', () => {
+        const configMock = {
+            dbNameMock: undefined,
+            dbUser: 'SAMUsak',
+            port: "8000"
+        };
+        const messageErrMock = 'Server cannot be initialized because it needs an environment variable.';
+        const expectedResult = new Error(messageErrMock);
+        expect(() => { (0, validatorEnv_1.default)(configMock); }).toThrow(expectedResult);
+    });
+    it('should return undefined or null', () => {
+        const configMock = {
+            dbNameMock: 'SMAEsa',
+            dbUser: 'SAMUsak',
+            port: "8000"
+        };
+        let expectResult;
+        expect((0, validatorEnv_1.default)(configMock)).toBe(expectResult);
+    });
+});
 //# sourceMappingURL=validatorEnv.test.js.map
--- test/validatorEnv.test.ts
+++ test/validatorEnv.test.ts
@@ -1,6 +1,40 @@
-const configMock = {
-  dbNameMock: '',
-  dbUser: 'SAMUsak',
-  port: 8000
-}
+import validatorEnv from '../utils/validatorEnv'
 
+
+
+describe('validatorEnv function', () => {
+  it('should return an error and a specific message', () => {
+    const configMock = {
+      dbNameMock: '',
+      dbUser: 'SAMUsak',
+      port: "8000"
+    }
+    const messageErrMock = 'Server cannot be initialized because it needs an environment variable.'
+
+    const expectedResult = new Error(messageErrMock)
+    expect(() => { validatorEnv(configMock) }).toThrow(expectedResult)
+  })
+
+  it('should return an error and a specific message', () => {
+    const configMock = {
+      dbNameMock: undefined!,
+      dbUser: 'SAMUsak',
+      port: "8000"
+    }
+    const messageErrMock = 'Server cannot be initialized because it needs an environment variable.'
+
+    const expectedResult = new Error(messageErrMock)
+    expect(() => { validatorEnv(configMock) }).toThrow(expectedResult)
+  })
+
+  it('should return undefined or null', () => {
+    const configMock = {
+      dbNameMock: 'SMAEsa',
+      dbUser: 'SAMUsak',
+      port: "8000"
+    }
+
+    let expectResult: undefined | null;
+    expect(validatorEnv(configMock)).toBe(expectResult)
+  })
+})
